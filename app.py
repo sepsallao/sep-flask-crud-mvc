ec77128c90f902085ffe1a69093573889928bcef
sepsallao <hardinnidionisio@gmail.com>
2021-12-28 00:44:57 +0800

    finished structure, finish writing on app and config file (based from felipe plain english)

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_migrate import Migrate
+
+from models.User import db
+from routes.user_bp import user_bp
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+app.register_blueprint(user_bp, url_prefix='/users')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main)__':
+    app.run(debug = True, host='0.0.0.0')
